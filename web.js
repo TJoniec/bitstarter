7229f6f074c0d501d23610c66f167ee62a37ec62
Tony Joniec <Tony Joniec@gmail.com>
2013-07-24 19:40:09 +0000

    Added fs.readfile function

--- web.js
+++ web.js
@@ -2,11 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var infile = fs.readfile('//index.html',,);
+
 app.get('/', function(request, response) {
-  response.send('Hello World2 !');
+    response.send(infile);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+    console.log("Listening on " + port);
