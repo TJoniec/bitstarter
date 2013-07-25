d708c7c3a7e742e0b57c3b8d981fe92d3cabf3c3
Tony Joniec <Tony Joniec@gmail.com>
2013-07-25 19:15:49 +0000

    Resolution to infile bug

--- web.js
+++ web.js
@@ -2,10 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var infile = fs.readFileSync('//index.html',,);
+var infile = fs.readFileSync('index.html',,);
 
 app.get('/', function(request, response) {
-    response.send(infile);
+    response.send('Works');
 });
 
 var port = process.env.PORT || 5000;
