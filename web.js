bf8953603eab32766aff561acd890f46707a82c7
Tony Joniec <Tony Joniec@gmail.com>
2013-07-25 19:57:42 +0000

    Added annotations and simplified code

--- web.js
+++ web.js
@@ -1,11 +1,15 @@
-var express = require('express');
+var express = require('express');  //import the express library
+var app = express(); //create the application
+app.use(express.logger());  // add some logging
 
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-    response.send('Works');
+// define a function to respond to the requests made to the site as an example
+app.get('/',function(request,response){
+    response.send('Hello World');
 });
 
+// start lisening on port 5000 and fire up the server
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
+app.listen(port, function(){
     console.log("Listening on " + port);
+});
+  
