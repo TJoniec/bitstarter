9022242fab6ec7c809438cc9bc596e09c6c95c9c
Tony Joniec <Tony Joniec@gmail.com>
2013-07-26 14:39:23 +0000

    Added readfile functionality

--- web.js
+++ web.js
@@ -5,9 +5,21 @@ var fs = require('fs'); //import the filesyatem library
 var app = express.createServer(); //create the application
 app.use(express.logger());  // add some logging
 
+
+
+// create a buffer to in order to hold the contents of the file to be read
+// and read it in at the same time.
+// Basic minimal form of the function call that could be made better by the using more options
+ 
+var file_buffer = fs.readFileSync('index.html');
+
+
+
 // define a function to respond to the requests made to the site as an example
+// buffer converts to a UTF string.
+
 app.get('/',function(request, response){
-    response.send('Hello World');
+    response.send(file_buffer.toString("utf-8");
 });
 
 // start lisening on port 5000 and fire up the server
